6da742a38b276c37daef3be7a72d3e0e6d27b73b
Aakash Srinivasan <86234579+Aakash-Srinivasan@users.noreply.github.com>
2025-05-01 23:32:56 +0530

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPA_BASE_TOKEN;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPA_BASE_CHAT_ID;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPA_BASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPA_BASE_PUBLIC_ANNON_KEY;
 
 export const supabase = createClient(supabaseUrl as any, supabaseAnonKey as any);
